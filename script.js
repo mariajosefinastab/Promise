e77e684b10f1febd7b2541f8d28d2bcfab22d236
mariajosefinastab <mariajosefinastabile@gmail.com>
2025-01-22 08:01:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ const myPromise = new Promise ((resolve, reject) =>{
     //llamadas a servicios
     //lectura o escritrura de archivos
 
-    
+    const random = Math.round(Math.random()*10)
 
     setTimeout(() => {
         const success = true
 
-        if(success){
-            resolve('Esta operación ha sido existosa');
+        if(random == number){
+            resolve('Felicitaciones! ganaste el juego');
+        }else if(random > number){
+            reject(`El número ${number} fue más pequeñño que ${random}`)
         }else{
-            reject('Ha fallado la operación');
+            reject(`El número ${number} fue más pequeñño que ${random}`)
         }
     }, 5000);
 });
@@ -27,6 +29,4 @@ myPromise.then(res => {
     document.getElementById('loading').innerHTML = ''
     document.getElementById('response').innerHTML = error
     //console.error(error)
-}).finally(
-    console.log('Esto se ejecuta siempre')
-)
+});
